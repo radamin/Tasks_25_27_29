b5d73de93c8d886619e801f2e4e8b42f71a51611
Ramazan Daminov <radamin@yandex.ru>
2023-01-16 19:03:41 +0300

    Added Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Inter number A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Inter number B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+int numB = numberB;
+int numA = numberA;
+
+
+if (numB > 0)
+{
+  for (int i = 1; i <= numB; i++)
+  {
+    result = result * numA;
+  }
+  Console.WriteLine("Answer: " + result);
+}
+else
+{
+  Console.Write("Error: enter a positive number");
+}
