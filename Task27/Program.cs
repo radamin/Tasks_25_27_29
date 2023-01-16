19dedaa3cca905a2ea3973aa3ce0546a75df3f85
Ramazan Daminov <radamin@yandex.ru>
2023-01-16 19:07:31 +0300

    Added Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Inter number:" + " ");
+int number = Convert.ToInt32(Console.ReadLine());
+int num = number;
+int sum = 0;
+
+if (num > 0)
+{
+  for (int i = 0; i <= num; i++)
+  {
+    sum += number % 10;
+    number /= 10;
+  }
+  Console.Write("The sum of the digits: " + sum);
+}
+else
+{
+  Console.Write("Error: enter a positive number");
+}
+
